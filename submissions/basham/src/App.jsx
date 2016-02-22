7eff133bfc976859b9c2855678e4d5187d85d150
Chris Basham <chris@bash.am>
2016-02-22 16:40:13 -0500

    Update: Initiate with starter HTML and CSS

--- submissions/basham/src/App.jsx
+++ submissions/basham/src/App.jsx
@@ -1,37 +1,42 @@
 import React, {Component} from 'react';
 
-import CounterReactExample from './examples/Counter.rx';
-import CounterRxExample from './examples/Counter.rx';
-
-import FocusReactExample from './examples/Focus.react';
-import FocusRxExample from './examples/Focus.rx';
-
-import IntervalReactExample from './examples/Interval.react';
-import IntervalRxExample from './examples/Interval.rx';
-
-import LifecycleReactExample from './examples/Lifecycle.react';
-
-import styles from './App.less';
+import styles from './styles.css';
 
 export default class App extends Component {
   render() {
     return (
-      <div className={styles.App}>
-        <h1>RxJS Proof of Concept examples</h1>
-        <h2>Counter (Rx)</h2>
-        <CounterRxExample/>
-        <h2>Counter (React)</h2>
-        <CounterReactExample/>
-        <h2>Focus (Rx)</h2>
-        <FocusRxExample/>
-        <h2>Focus (React)</h2>
-        <FocusReactExample/>
-        <h2>Interval (Rx)</h2>
-        <IntervalRxExample/>
-        <h2>Interval (React)</h2>
-        <IntervalReactExample/>
-        <h2>Lifecycle (React)</h2>
-        <LifecycleReactExample/>
+      <div className={styles['app-container']}>
+        <div className={styles['css-root']}>
+          <h1 className={styles['css-planet-monitor']}>Obi-Wan currently on Tatooine</h1>
+          <section className={styles['css-scrollable-list']}>
+            <ul className={styles['css-slots']}>
+              <li className={styles['css-slot']}>
+                <h3>Jorak Uln</h3>
+                <h6>Homeworld: Korriban</h6>
+              </li>
+              <li className={styles['css-slot']}>
+                <h3>Skere Kaan</h3>
+                <h6>Homeworld: Coruscant</h6>
+              </li>
+              <li className={styles['css-slot']}>
+                <h3>Na'daz</h3>
+                <h6>Homeworld: Ryloth</h6>
+              </li>
+              <li className={styles['css-slot']}>
+                <h3>Kas'im</h3>
+                <h6>Homeworld: Nal Hutta</h6>
+              </li>
+              <li className={styles['css-slot']}>
+                <h3>Darth Bane</h3>
+                <h6>Homeworld: Apatros</h6>
+              </li>
+            </ul>
+            <div className={styles['css-scroll-buttons']}>
+              <button className={styles['css-button-up']}></button>
+              <button className={styles['css-button-down']}></button>
+            </div>
+          </section>
+        </div>
       </div>
     );
   }
