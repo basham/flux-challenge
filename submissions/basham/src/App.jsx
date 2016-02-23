ad0c61154f686c37ee25afd7a556dcb7ed436021
Chris Basham <chris@bash.am>
2016-02-23 16:21:05 -0500

    Update: Fetch initial dark jedi, render from slot list

--- submissions/basham/src/App.jsx
+++ submissions/basham/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import {Component, FuncSubject} from 'rx-react';
 import Rx from 'rx';
+import superagent from 'superagent';
 
 import styles from './styles.css';
+
+const WS_PATH = 'ws://localhost:4000';
+const API_PATH = 'http://localhost:3000';
+const DARTH_SIDIOUS_ID = 3616;
 
 function currentPlanet(url) {
   const connection = new WebSocket(url);
@@ -17,11 +22,36 @@ function currentPlanet(url) {
   return ws;
 }
 
-function model(planet$) {
-  return planet$
+function fetchDarkJedi(id) {
+  const req = superagent
+    .get(`${API_PATH}/dark-jedis/${id}`);
+  return Rx.Observable
+    .fromNodeCallback(req.end, req)()
+}
+
+function model(planetResponse$, darkJedi$) {
+  darkJedi$
+    .subscribe(x => console.log('---', x));
+
+  const planet$ = planetResponse$
     .map(({data}) => JSON.parse(data))
-    .startWith(null)
-    .map((planet) => ({planet}));
+    .startWith(null);
+
+  const slots$ = Rx.Observable
+    .just([
+      {name: 'Jorak Uln', homeworld: {name: 'Korriban'}, id: 0},
+      {name: 'Skere Kaan', homeworld: {name: 'Coruscant'}, id: 1},
+      {name: 'Na\'daz', homeworld: {name: 'Ryloth'}, id: 2},
+      {name: 'Kas\'im', homeworld: {name: 'Nal Hutta'}, id: 3},
+      {name: 'Darth Bane', homeworld: {name: 'Apatros'}, id: 4}
+    ]);
+
+  return Rx.Observable
+    .combineLatest(
+      planet$,
+      slots$,
+      (planet, slots) => ({planet, slots})
+    );
 }
 
 function view(state) {
@@ -36,26 +66,7 @@ function view(state) {
         </h1>
         <section className={styles['css-scrollable-list']}>
           <ul className={styles['css-slots']}>
-            <li className={styles['css-slot']}>
-              <h3>Jorak Uln</h3>
-              <h6>Homeworld: Korriban</h6>
-            </li>
-            <li className={styles['css-slot']}>
-              <h3>Skere Kaan</h3>
-              <h6>Homeworld: Coruscant</h6>
-            </li>
-            <li className={styles['css-slot']}>
-              <h3>Na'daz</h3>
-              <h6>Homeworld: Ryloth</h6>
-            </li>
-            <li className={styles['css-slot']}>
-              <h3>Kas'im</h3>
-              <h6>Homeworld: Nal Hutta</h6>
-            </li>
-            <li className={styles['css-slot']}>
-              <h3>Darth Bane</h3>
-              <h6>Homeworld: Apatros</h6>
-            </li>
+            {state.slots.map(renderSlot)}
           </ul>
           <div className={styles['css-scroll-buttons']}>
             <button className={styles['css-button-up']}></button>
@@ -67,10 +78,24 @@ function view(state) {
   );
 }
 
+function renderSlot({homeworld, id, name}) {
+  return (
+    <li
+      className={styles['css-slot']}
+      key={id}>
+      <h3>{name}</h3>
+      <h6>Homeworld: {homeworld.name}</h6>
+    </li>
+  )
+}
+
 export default class App extends Component {
   getStateStream() {
-    const planet$ = currentPlanet('ws://localhost:4000');
-    return model(planet$);
+    const fetch$ = Rx.Observable
+      .just(DARTH_SIDIOUS_ID)
+      .flatMap((id) => fetchDarkJedi(id))
+    const planet$ = currentPlanet(WS_PATH);
+    return model(planet$, fetch$);
   }
   render() {
     return view(this.state);
