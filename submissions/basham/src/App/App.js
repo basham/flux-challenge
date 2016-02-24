84899b3dd2bb91b1fb003cc52f259257974fc99a
Chris Basham <chris@bash.am>
2016-02-24 17:13:09 -0500

    Update: Fetch sith from responses

--- submissions/basham/src/App/App.js
+++ submissions/basham/src/App/App.js
@@ -9,7 +9,7 @@ const API_PATH = 'http://localhost:3000';
 const DARTH_SIDIOUS_ID = 3616;
 const SLOT_COUNT = 5;
 
-function currentPlanet(url) {
+function fetchCurrentPlanet(url) {
   const connection = new WebSocket(url);
   const ws = FuncSubject.create();
   connection.onerror = (err) => {
@@ -27,6 +27,14 @@ function fetchDarkJedi(id) {
     .get(`${API_PATH}/dark-jedis/${id}`);
   return Rx.Observable
     .fromNodeCallback(req.end, req)()
+}
+
+function darkJediHTTP() {
+  const request = FuncSubject.create();
+  const response$ = request
+    .startWith(DARTH_SIDIOUS_ID)
+    .flatMap((id) => fetchDarkJedi(id));
+  return {request, response$};
 }
 
 function model(planetResponse$, darkJediResponse$) {
@@ -57,21 +65,40 @@ function model(planetResponse$, darkJediResponse$) {
       initialSlots
     );
 
-  return Rx.Observable
+  const state$ = Rx.Observable
     .combineLatest(
       planet$,
       slots$,
       (planet, slots) => ({planet, slots})
     );
+
+  return {slots$, state$};
+}
+
+function fetchMoreDarkJedi(slots$, request) {
+  slots$
+    // Loop through each slots.
+    .flatMap((slots) =>
+      slots
+        // Find only slot items with content.
+        .filter(sith => sith !== null)
+        // Extract every apprentice and master combo into array.
+        .map(({apprentice, master}) => [apprentice, master])
+        // Flatten the array of arrays into a single array.
+        .reduce((a, b) => a.concat(b), [])
+        // Keep only the id of each sith.
+        .map(({id}) => id)
+    )
+    //.subscribe((id) => request(id))
 }
 
 export default class App extends Component {
   getStateStream() {
-    const fetch$ = Rx.Observable
-      .just(DARTH_SIDIOUS_ID)
-      .flatMap((id) => fetchDarkJedi(id))
-    const planet$ = currentPlanet(WS_PATH);
-    return model(planet$, fetch$);
+    const darkJedi = darkJediHTTP();
+    const planetResponse$ = fetchCurrentPlanet(WS_PATH);
+    const {slots$, state$} = model(planetResponse$, darkJedi.response$);
+    fetchMoreDarkJedi(slots$, darkJedi.request);
+    return state$;
   }
   render() {
     return view(this.state);
